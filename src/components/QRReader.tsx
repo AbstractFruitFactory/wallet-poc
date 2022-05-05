96132dcd9cd10bda019b5ec65b5f5b3c5de059a7
AbstractFruitFactory <>
2022-05-05 11:13:40 +0100

    add qr

---
+++ src/components/QRReader.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import { QrReader } from "react-qr-reader";
+
+export const QRReader = () => {
+  const [data, setData] = useState("No result");
+
+  useEffect(() => {
+    (async () => {
+      const parsedData = JSON.parse(data);
+
+      const peerConnection = new RTCPeerConnection({
+        iceServers: [
+          { urls: "stun:stun2.l.google.com:19302" },
+          { urls: "stun:stun.vavadating.com:3478" },
+        ],
+      });
+
+      const description = await peerConnection.createAnswer();
+
+      peerConnection.setLocalDescription(description);
+      peerConnection.setRemoteDescription(parsedData.description);
+      peerConnection.addIceCandidate(parsedData.ice);
+
+      fetch(`${parsedData.signalingURL}/answer`, {
+        method: "POST",
+        body: JSON.stringify(description),
+      });
+    })();
+  }, [data]);
+
+  return (
+    <>
+      <QrReader
+        onResult={(result, error) => {
+          if (!!result) {
+            // @ts-ignore
+            setData(result?.text);
+          }
+
+          if (!!error) {
+            console.info(error);
+          }
+        }}
+        // @ts-ignore
+        style={{ width: "100%" }}
+      />
+      <p>{data}</p>
+    </>
+  );
+};
